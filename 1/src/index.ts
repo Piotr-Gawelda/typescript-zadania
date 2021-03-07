6722aba7332089ad7db7cd0aea2733fe8a2f5442
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-03-07 11:20:47 +0100

    first task

---
+++ 1/src/index.ts
@@ -0,0 +1,17 @@
+const sumInput = document.querySelector("#sum");
+
+const firstInput = document.querySelector("#firstInput");
+
+const secInput = document.querySelector("#secInput");
+
+const thirdInput = document.querySelector("#thirdInput");
+
+const fourthInput = document.querySelector("#fourthInput");
+
+function sum(first: number, sec: number, third: number, fourth: number) {
+    return first + sec + third + fourth;
+}
+
+// sumInput.addEventListener("onchange", function() {
+    
+// })
