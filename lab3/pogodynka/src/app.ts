758d44444d2343afeeb321fc0329dcc6015312b9
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-10-25 15:50:18 +0200

    weather app update

--- lab3/pogodynka/src/app.ts
+++ lab3/pogodynka/src/app.ts
@@ -1,11 +1,11 @@
 export class App {
-    opwApiKey = '7019fefcaec80abfb112bf502e298791';
+    opwApiKey = '';
     constructor() {
         this.getCityInfo('zakopane')
     }
     async getCityInfo(city: string) {
-        const weather = await this.getWeather('zakopane');
-        this.saveData(weather);
+        // const weather = await this.getWeather('zakopane');
+        // this.saveData(weather);
     }
     async getWeather(city: string): Promise<any> {
         const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
