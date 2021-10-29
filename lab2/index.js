13952b52bb624fb99e298b92879ab1763edd4d04
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-10-29 16:15:48 +0200

    drumkit update

--- lab2/index.js
+++ lab2/index.js
@@ -20,9 +20,103 @@ var Channel = /** @class */ (function () {
 }());
 var App = /** @class */ (function () {
     function App() {
+        this.sounds = {};
+        this.activeRecord = -1;
+        this.records = [];
+        this.channels = [];
         this.appStart();
     }
     App.prototype.appStart = function () {
+        var _this = this;
+        document.addEventListener('keypress', function (event) { return _this.onKeyPress(event); });
+        this.getAudioElements();
+        this.getChannelsElements();
+    };
+    App.prototype.getChannelsElements = function () {
+        var _this = this;
+        var channels = document.querySelectorAll('[data-channel]');
+        channels.forEach(function (x, index) {
+            var ind = index + 1;
+            var record = x.querySelector("#record-" + ind);
+            record.addEventListener('click', function () { return _this.recordClick(ind); });
+            _this.records.push(record);
+            var note = x.querySelector("#notes-" + ind);
+            var play = x.querySelector("#play-" + ind);
+            play.addEventListener('click', function () { return _this.onPlayChannel(ind); });
+            _this.channels.push(new Channel(ind, record, note));
+        });
+    };
+    App.prototype.recordClick = function (id) {
+        var channel = this.channels.find(function (x) { return x.id === id; });
+        if (this.activeRecord === id) {
+            this.activeRecord = -1;
+            channel.record.classList.remove("active");
+            return;
+        }
+        this.activeRecord = id;
+        this.records.forEach(function (x) {
+            if (x.classList)
+                x.classList.remove("active");
+        });
+        channel.record.classList.add("active");
+    };
+    App.prototype.onPlayChannel = function (index) {
+        var channel = this.channels.find(function (x) { return x.id === index; });
+        var index = 0;
+        if (channel.notes.length > 0) {
+            this.playOneElement(channel.notes, index);
+        }
+    };
+    App.prototype.playOneElement = function (keys, index) {
+        var _this = this;
+        if (index >= keys.length) {
+            return;
+        }
+        var audio = this.sounds[keys[index]];
+        index++;
+        audio.element.onended = function () {
+            audio.element.onended = null;
+            _this.playOneElement(keys, index);
+        };
+        audio.play();
+    };
+    App.prototype.getAudioElements = function () {
+        var _this = this;
+        var audios = document.querySelectorAll('[data-sound-key]');
+        audios.forEach(function (x) {
+            var key = x.getAttribute('data-sound-key');
+            var audio = _this.getAudio(x, key);
+            _this.sounds[key] = audio;
+        });
+        audios = document.querySelectorAll('[data-key]');
+        audios.forEach(function (x) {
+            var key = x.getAttribute('data-key');
+            x.addEventListener("click", function () { return _this.playSound(key); });
+        });
+    };
+    App.prototype.getAudio = function (x, y) {
+        return new AudioElement(x, y);
+    };
+    App.prototype.onKeyPress = function (ev) {
+        var key = ev.key;
+        this.playSound(key);
+    };
+    App.prototype.playSound = function (key) {
+        var _this = this;
+        var elem = this.sounds[key];
+        if (elem) {
+            elem.play();
+        }
+        else {
+            return;
+        }
+        if (this.activeRecord !== -1) {
+            var channelId = this.channels.findIndex(function (x) { return x.id === _this.activeRecord; });
+            this.channels[channelId].notes.push(key);
+            var elemSpan = document.createElement("span");
+            elemSpan.innerText = key.toUpperCase();
+            this.channels[channelId].note.appendChild(elemSpan);
+        }
     };
     return App;
 }());
--- lab2/index.ts
+++ lab2/index.ts
@@ -29,12 +29,116 @@ class Channel {
 
 class App {
 
+    sounds: {
+        [id: string]: AudioElement;
+    } = {};
+
+    activeRecord: Number = -1;
+    records: Element[] = [];
+
+    channels: Channel[] = [];
     constructor() {
         this.appStart();
     }
 
     appStart() {
+        document.addEventListener('keypress', event => this.onKeyPress(event));
+        this.getAudioElements();
+        this.getChannelsElements();
+    }
 
+    getChannelsElements() {
+        var channels = document.querySelectorAll('[data-channel]');
+        channels.forEach((x, index) => {
+            var ind = index + 1;
+            var record = x.querySelector("#record-" + ind);
+            record.addEventListener('click', () => this.recordClick(ind));
+            this.records.push(record);
+            var note = x.querySelector("#notes-" + ind);
+            var play = x.querySelector("#play-" + ind);
+            play.addEventListener('click', () => this.onPlayChannel(ind));
+            this.channels.push(new Channel(ind, record, note));
+        });
+    }
+
+    recordClick(id: Number) {
+        var channel = this.channels.find(x => x.id === id);
+
+        if (this.activeRecord === id) {
+            this.activeRecord = -1;
+            channel.record.classList.remove("active");
+            return;
+        }
+
+        this.activeRecord = id;
+        this.records.forEach(x => {
+            if (x.classList)
+                x.classList.remove("active");
+        })
+        channel.record.classList.add("active");
+    }
+
+    onPlayChannel(index: number): void {
+        var channel = this.channels.find(x => x.id === index);
+        var index = 0;
+        if (channel.notes.length > 0) {
+            this.playOneElement(channel.notes, index);
+        }
+    }
+
+    playOneElement(keys: string[], index: number) {
+        if (index >= keys.length) {
+            return;
+        }
+
+        var audio = this.sounds[keys[index]];
+        index++;
+        audio.element.onended = () => {
+            audio.element.onended = null;
+            this.playOneElement(keys, index)
+        }
+        audio.play();
+    }
+
+    getAudioElements(): void {
+        var audios = document.querySelectorAll('[data-sound-key]');
+        audios.forEach(x => {
+            var key = x.getAttribute('data-sound-key');
+            const audio = this.getAudio(x, key);
+            this.sounds[key] = audio;
+        });
+
+        audios = document.querySelectorAll('[data-key]');
+        audios.forEach(x => {
+            var key = x.getAttribute('data-key');
+            x.addEventListener("click", () => this.playSound(key));
+        });
+    }
+
+    getAudio(x: any, y: any): AudioElement {
+        return new AudioElement(x, y);
+    }
+
+
+    onKeyPress(ev: KeyboardEvent): void {
+        const key = ev.key;
+        this.playSound(key);
+    }
+
+    playSound(key: string) {
+        var elem = this.sounds[key];
+        if (elem) {
+            elem.play();
+        } else {
+            return;
+        }
+        if (this.activeRecord !== -1) {
+            var channelId = this.channels.findIndex(x => x.id === this.activeRecord);
+            this.channels[channelId].notes.push(key);
+            var elemSpan = document.createElement("span");
+            elemSpan.innerText = key.toUpperCase();
+            this.channels[channelId].note.appendChild(elemSpan);
+        }
     }
 }
 const app = new App();
