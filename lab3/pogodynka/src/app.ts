b54b4ffc9d2898de4bed51a5a6666beeeb13df56
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-10-29 11:49:36 +0200

    weather app update

--- lab3/pogodynka/src/app.ts
+++ lab3/pogodynka/src/app.ts
@@ -1,28 +1,58 @@
 export class App {
-    opwApiKey = '';
+    opwApiKey = '7cf6da23f61dff7703a2d327a9298c7d';
     constructor() {
-        this.getCityInfo('zakopane')
+        const searchButton = document.querySelector("#search");
+        searchButton.addEventListener("click", () => {
+            this.getCityInfo()
+        });
+        this.getData();
     }
-    async getCityInfo(city: string) {
-        // const weather = await this.getWeather('zakopane');
-        // this.saveData(weather);
+
+    async getCityInfo() {
+        const city = document.querySelector("#city") as HTMLInputElement;
+        const weather = await this.getWeather(city.value);
+        if (weather) {
+            this.displayData(weather);
+            this.saveData(weather);
+        }
     }
-    async getWeather(city: string): Promise<any> {
-        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+
+    async getWeather(city: string): Promise < any > {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}&units=metric`;
         const weatherResponse = await fetch(openWeatherUrl);
-        const weatherData = await weatherResponse.json();
-        console.log(weatherData);
-        return weatherData;
+        if (weatherResponse.status === 200) {
+            const weatherData = await weatherResponse.json();
+            return weatherData;
+        }
+        return null;
     }
+
+    displayData(data: any) {
+        const template = `<div class="city-name">${data.name}</div><div class="weather">${data.weather[0].main}</div><div class="sub-info"><div class="temperature">${data.main.temp}&#176;C</div><div class="sub-info-2"><div>Ciśnienie</div><div>${data.main.pressure} hPA</div><hr><div>Wilgotność</div><div>${data.main.humidity} %</div></div></div>`;
+        const container = document.querySelector("#weather-container");
+        const elem = document.createElement("div");
+        elem.classList.add("weather-box");
+        elem.innerHTML = template.trim();
+        container.prepend(elem);
+    }
+
     saveData(data: any) {
-        localStorage.setItem('weatherData', JSON.stringify(data));
+        const weathers = localStorage.getItem('weatherData');
+        var weathersObj = [];
+        if (weathers) {
+            weathersObj = JSON.parse(weathers) as any[];
+        }
+        weathersObj.push(data);
+        localStorage.setItem('weatherData', JSON.stringify(weathersObj));
     }
+
     getData() {
         const data = localStorage.getItem('weatherData');
         if (data) {
-            return JSON.parse(data);
-        } else {
-            return {};
+            const weathersObj = JSON.parse(data) as any[];
+            weathersObj.forEach(x => {
+                this.displayData(x);
+            });
         }
     }
 }
